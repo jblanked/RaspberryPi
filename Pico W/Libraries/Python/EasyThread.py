aad4f3718104980bb482ce6278d2e7126f04129d
jblanked <82678820+jblanked@users.noreply.github.com>
2024-11-14 23:02:09 -0500

    Update EasyThread.py

--- Pico W/Libraries/Python/EasyThread.py
+++ Pico W/Libraries/Python/EasyThread.py
@@ -1,55 +1,55 @@
 import _thread
+import sys
 
 
 class EasyThread:
-    def __init__(self, target, args=(), kwargs={}, lock_thread=False):
+    def __init__(self, target, args=(), kwargs=None, lock_thread=False):
+        if kwargs is None:
+            kwargs = {}
         self.target = target
-        self.args = args
+        self.args = list(args)
         self.kwargs = kwargs
         self.thread = None
         self.lock_alloc = _thread.allocate_lock()
         self.lock_thread = lock_thread
+        self.should_stop = False  # Flag to signal the thread to stop
 
     def start(self):
-        # Start the thread and store its reference
+        self.should_stop = False  # Reset the stop flag
         self.thread = _thread.start_new_thread(self.run, ())
 
     def run(self):
-        # Run the target function
-        self.target(*self.args, **self.kwargs)
-        # Mark the thread as finished
-        self.thread = None
+        try:
+            # Run the target function and periodically check if it should stop
+            if not self.should_stop:
+                self.target(*self.args, **self.kwargs)
+        except Exception as e:
+            print(f"Thread encountered an error: {e}")
+        finally:
+            self.thread = None  # Mark thread as finished
+            sys.exit()  # Exit the system thread
+
+    def end(self):
+        self.should_stop = True  # Signal the thread to stop
 
     def join(self):
-        # Busy-wait until the thread is finished
         while self.is_alive():
             pass
 
     def is_alive(self):
         return self.thread is not None
 
-    def terminate(self):
-        # Reset the thread reference to terminate
-        self.thread = None
-
-    def lock(self):
-        self.lock_alloc.acquire()
-
-    def unlock(self):
-        self.lock_alloc.release()
-
     def __del__(self):
-        self.terminate()
+        self.end()
 
     def __enter__(self):
-        # Acquire the lock and start the thread
         if self.lock_thread:
-            self.lock()
+            self.lock_alloc.acquire()
         self.start()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        # Wait for the thread to finish and release the lock
+        self.end()
         self.join()
         if self.lock_thread:
-            self.unlock()
+            self.lock_alloc.release()
