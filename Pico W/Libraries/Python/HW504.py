188e2ddd05b98a2eefe7758b1f7f4fc51a0f1379
jblanked <82678820+jblanked@users.noreply.github.com>
2025-01-16 13:00:06 -0500

    add orientation + value method

--- Pico W/Libraries/Python/HW504.py
+++ Pico W/Libraries/Python/HW504.py
@@ -5,38 +5,77 @@ from machine import Pin, ADC
 # VRx -> GP27 (Pin 32 - ADC1)
 # VRy -> GP26 (Pin 31 - ADC0)
 # GND -> GND
-# 5V -> 3v3 (Pin 36)
+# 5V -> VSYS (Pin 39)
 
-# X-axis
-# 0 - 10000: left
-# 20000 - 33000: middle
-# 40000 - 65000: right
+HW_LEFT_BUTTON = 0
+HW_RIGHT_BUTTON = 1
+HW_UP_BUTTON = 2
+HW_DOWN_BUTTON = 3
+HW_CENTER_BUTTON = 4
 
-# Y-axis
-# 0 - 10000: up
-# 20000 - 33000: middle
-# 40000 - 65000: down
+HW_ORIENTATION_NORMAL = 0
+HW_ORIENTATION_90 = 1
+HW_ORIENTATION_180 = 2
+HW_ORIENTATION_270 = 3
 
 
 class HW504:
-    def __init__(self, x_pin=27, y_pin=26, button_pin=17):
+    def __init__(
+        self, x_pin=27, y_pin=26, button_pin=17, orientation=HW_ORIENTATION_NORMAL
+    ):
         self.x_axis = ADC(Pin(x_pin))
         self.y_axis = ADC(Pin(y_pin))
         self.button = Pin(button_pin, Pin.IN, Pin.PULL_UP)
+        self.orientation = orientation
 
-    def _x_axis(self):
+    def _x_axis(self) -> int:
         """Read the x-axis value from the joystick."""
-        return self.x_axis.read_u16()
+        normal_x_axis = self.x_axis.read_u16()
+        if self.orientation in [HW_ORIENTATION_90, HW_ORIENTATION_270]:
+            return normal_x_axis
+        return 65535 - normal_x_axis
 
-    def _y_axis(self):
+    def _y_axis(self) -> int:
         """Read the y-axis value from the joystick."""
-        return self.y_axis.read_u16()
+        normal_y_axis = self.y_axis.read_u16()
+        if self.orientation in [HW_ORIENTATION_90, HW_ORIENTATION_NORMAL]:
+            return normal_y_axis
+        return 65535 - normal_y_axis
 
-    def _button(self):
+    def _button(self) -> int:
         """Read the button value from the joystick."""
         return self.button.value()
 
-    def read(self, stringify=False):
+    def value(self, button: int = HW_CENTER_BUTTON) -> bool:
+        """
+        Return the state of a button based on the x,y axis values
+
+        X-axis:
+        0 - 10000: left
+        20000 - 33000: middle
+        40000 - 65000: right
+
+        Y-axis:
+        0 - 10000: up
+        20000 - 33000: middle
+        40000 - 65000: down
+        """
+        x = self._x_axis()
+        y = self._y_axis()
+        if button == HW_LEFT_BUTTON:
+            return x < 10000
+        if button == HW_RIGHT_BUTTON:
+            return x > 40000
+        if button == HW_UP_BUTTON:
+            return y < 10000
+        if button == HW_DOWN_BUTTON:
+            return y > 40000
+        if button == HW_CENTER_BUTTON:
+            return self._button() == 0
+
+        return False
+
+    def read(self, stringify=False) -> dict:
         """Read the x-axis, y-axis and button values from the joystick as a dictionary."""
         return {
             "x": self._x_axis(),
